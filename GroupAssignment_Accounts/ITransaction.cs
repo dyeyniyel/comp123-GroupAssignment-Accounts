8d7fd8a61036f70914ffa5da83cd59e0f65b1996
Janiel Mark Javier <jjavie27@my.centennialcollege.ca>
2024-04-02 01:56:22 -0400

    Add project files.

---
+++ GroupAssignment_Accounts/ITransaction.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using static GroupAssignment_Accounts.MainAccount;
+
+public interface ITransaction
+{
+    void Withdraw(double amount, Person person);
+    void Deposit(double amount, Person person);
+}
